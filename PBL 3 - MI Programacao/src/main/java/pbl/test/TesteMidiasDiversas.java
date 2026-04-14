e95037ae37fabd965a21ea27dad41cb6aae19f6b
Enzo Cauã <caua7uefs@gmail.com>
2026-04-14 13:36:08 -0300

    Update and rename TesteMidiasDiversa.java to TesteMidiasDiversas.java

--- PBL 3 - MI Programacao/src/main/java/pbl/test/TesteMidiasDiversas.java
+++ PBL 3 - MI Programacao/src/main/java/pbl/test/TesteMidiasDiversas.java
@@ -6,7 +6,7 @@ import pbl.model.persistencia.PersistirAcervo;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class TesteMidiasDiversa {
+public class TesteMidiasDiversas {
 
     public static void main(String[] args) {
         PersistirAcervo.deserializarAcervo(); // Carrega o acervo existente
